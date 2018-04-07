dbc3cbac4b7b0bc825c540b9dc1b16a9f888d659
Giovanna Anacleto <giovana.anacleto@al.infnet.edu.br>
2018-04-07 20:43:33 +0000

    Padronizando cabeçalho e rodapé e fazendo página de cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './index/index.component';
-
+import {UsuarioComponent} from './usuario/usuario.component';
 const routes: Routes = [
   {path:'index', component : IndexComponent},
+  {path: 'usuario', component : UsuarioComponent},
   {path:'**', component : IndexComponent}
 ];
 
