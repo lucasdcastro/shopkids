69f941e0e3ca02a00984053a9d9433e577d6aed1
lucasdcastro <lucasdcastrocf@outlook.com.br>
2018-04-15 23:12:42 -0300

    instalando json-server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {ApiserviceService} from "./apiservice.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ApiserviceService, HttpClient]
 })
 export class AppComponent {
+
   title = 'app';
+  login = '';
+  senha = '';
+
+  constructor(private chamadaApi : ApiserviceService,
+              private rota: Router,
+              public web : HttpClient) { }
+
+  ngOnInit() {
+  }
+
+  realizarLogin(email, senha) {
+    this.chamadaApi.buscarUsuario(email, senha).subscribe (retornoApi => {
+      console.log(retornoApi);
+      this.chamadaApi.salvarUsuario(JSON.stringify(retornoApi));
+    });
+  }
+
 }
