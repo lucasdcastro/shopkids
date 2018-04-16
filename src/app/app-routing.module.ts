462d27473c22c2973f00b795114f6772240fcb08
lucasdcastro <lucasdcastrocf@outlook.com.br>
2018-04-16 01:11:32 -0300

    json-server

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import {UsuarioComponent} from './usuario/usuario.component';
+import {ProdutosComponent} from './produtos/produtos.component';
 import {AcessoriosComponent} from './produtos/acessorios/acessorios.component';
 import {BrinquedosComponent} from './produtos/brinquedos/brinquedos.component';
 import {MoveisComponent} from './produtos/moveis/moveis.component';
@@ -10,6 +11,7 @@ import {RoupasComponent} from './produtos/roupas/roupas.component';
 const routes: Routes = [
   {path:'index', component : IndexComponent},
   {path: 'usuario', component : UsuarioComponent},
+  {path: 'produtos', component : ProdutosComponent},
   {path: 'produtos/acessorios', component : AcessoriosComponent},
   {path: 'produtos/brinquedos', component : BrinquedosComponent},
   {path: 'produtos/moveis', component : MoveisComponent},
